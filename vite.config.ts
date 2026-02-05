acde4043357f01f8090e6251d5348d01837c248c
thaicvhnue-lgtm <thaicv.hnue@gmail.com>
2026-02-05 16:20:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+          base: '/GenesisSchool/',
         port: 3000,
         host: '0.0.0.0',
       },
